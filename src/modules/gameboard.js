6bd38b62e9736ec61655cc419c97d187c54644eb
Patryk Hasegawa <contact@p-hsgw.com>
2022-05-07 22:31:27 +0900

    Gameboard marks hit spots

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -3,20 +3,32 @@ const gameboardFactory = () => {
 
     const populateGameBoard = () => {
         obj.array = Array.from(Array(10), () => {
-            return new Array(10).fill(0)
+            return new Array(10).fill(null)
         })
     }
 
     const placeShip = (ship, coordinateX, coordinateY) => {
         obj.array[coordinateX][coordinateY] = 1
+        // Check ship length, and iterate over the arr to place as many fields as possible
+        // If ship is reversed, change the loop
     }
 
     const receiveAttack = (coordinateX, coordinateY) => {
+        if (obj.array[coordinateX][coordinateY] = 1) {
+            console.log("sinked")
+            obj.array[coordinateX][coordinateY] = "X"
+        } else {
+            console.log("missed")
+            obj.array[coordinateX][coordinateY] = "X"
+        }
         // Ship receives attack if clicked on coordinate
         // If coordinate of ship == attack coordinate, damage ship
         // Else mark the missed spot
+
+        // If field is 1, mark it as damage for the ship
+        // If field is null, mark it as X and not clickable
     }
-    let obj = {array, populateGameBoard, placeShip}
+    let obj = {array, populateGameBoard, placeShip, receiveAttack}
     return obj
 }
 export default gameboardFactory
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -8,35 +8,37 @@ const ship1 = shipFactory(1)
 
 test("Gameboard populates itself", () =>{
     expect(gameboard.array).toStrictEqual([ 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
       ])
 })
 
 test("The gameboard places a ship on 1, 1 field", () => {
-    gameboard.placeShip("ship", 1, 1)
+    gameboard.placeShip(ship1, 1, 1)
     expect(gameboard.array).toStrictEqual([ 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],        
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, 1, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null],        
     ])
 })
 
-// test("Gameboard receives attack and marks correct spot",() => {
-//     expect(gameboard()).toBe("XXX")
-// })
+test("Gameboard receives attack and marks correct spot", () => {
+    gameboard.placeShip(ship1, 1, 1)
+    gameboard.receiveAttack(1, 1)
+    expect(gameboard.array[1][1]).toBe("X")
+})
